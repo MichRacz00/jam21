cf1ba3dec8d8c9cb6ccb90b20b4d5cc17815055f
Michal <michal.raczkiewicz@hotmail.com>
2025-01-08 12:14:01 +0100

    RA relation added correctly to the global relation object

--- src/VOCalculator.cpp
+++ src/VOCalculator.cpp
@@ -1,52 +1,83 @@
-/*
- * GenMC -- Generic Model Checking.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, you can access it online at
- * http://www.gnu.org/licenses/gpl-3.0.html.
- *
- * Author: Michalis Kokologiannakis <michalis@mpi-sws.org>
- */
-
 #include "VOCalculator.hpp"
 #include "Error.hpp"
 #include "ExecutionGraph.hpp"
 #include "GraphIterators.hpp"
+
+void VOCalculator::initCalc()
+{	
+	initRaRelation();
+	return;
+}
+
+Calculator::CalculationResult VOCalculator::doCalc()
+{
+	llvm::outs() << "--------------- Called doCalc() ---------------\n";
+
+	auto &g = getGraph();
+	auto &raRelation = g.getGlobalRelation(ExecutionGraph::RelationId::ra);
+
+	calcRaRelation();
+	llvm::outs() << raRelation << "\n";
+
+	return Calculator::CalculationResult(false, false);
+}
+
+void VOCalculator::removeAfter(const VectorClock &preds)
+{
+	/* We do not track anything specific */
+	return;
+}
+
+/**
+ * Retrieves n previous events starting from and including the given event,
+ * and returns them in a vector. The events are ordered from the most recent 
+ * (largest timestamp) to the oldest (smallest timestamp), with the given 
+ * event at the start of the vector. If fewer than n previous events are available, 
+ * an empty vector is returned.
+ */
+
+std::vector<std::unique_ptr<EventLabel>> VOCalculator::getPrevMany(const EventLabel *lab, int n) {
+	auto &g = getGraph();
+	std::vector<std::unique_ptr<EventLabel>> labels;
+	auto currentLab = lab;
+
+    while (n > 0) {
+        labels.push_back(currentLab->clone());
+        auto prevLab = g.getPreviousNonEmptyLabel(currentLab);
+		/*
+		* If the previous label is the same as the current label, and there are
+        * more labels left to retrieve, return an empty vector indicating
+        * insufficient previous labels.
+		*/ 
+		if (prevLab == currentLab && n > 1) return {};
+		currentLab = prevLab;
+        --n;
+    }
+
+    return labels;
+}
 
 void VOCalculator::calcRaRelation() {
 	auto &g = getGraph();
 	auto &raRelation = g.getGlobalRelation(ExecutionGraph::RelationId::ra);
 
 	for (const auto *lab : labels(g)) {
-		auto thirdEventLabel = lab;
-		auto secondEventLabel = g.getPreviousNonEmptyLabel(thirdEventLabel);
-		auto firstEventLabel = g.getPreviousNonEmptyLabel(secondEventLabel);
+		auto events = getPrevMany(lab, 3);
+		if (events.empty()) continue;
 
-		// If any two relations have the same stamp,
-		// that means no more previous events in this thread,
-		// thus relation cannot exist
-		if (thirdEventLabel == secondEventLabel) continue;
-		if (secondEventLabel == firstEventLabel) continue;
+		auto thirdEventLabel = events[0].get();
+		auto secondEventLabel = events[1].get();
+		auto firstEventLabel = events[2].get();
 
 		// The event in the middle must be rel/acq or stronger
 		if (!(secondEventLabel->isAtLeastAcquire() || secondEventLabel->isAtLeastRelease())) continue;
-
-		raRelation.addEdge(firstEventLabel->getIndex(), thirdEventLabel->getIndex());
+		
+		raRelation.addEdge(firstEventLabel->getPos(), thirdEventLabel->getPos());
 	}
 }
 
-/*
-*	Initializes ra relation as relation possibly containing all events.
+/**
+ * Initializes ra relation as relation possibly containing all events.
 */
 void VOCalculator::initRaRelation() {
 	auto &g = getGraph();
@@ -58,33 +89,4 @@ void VOCalculator::initRaRelation() {
 
 	auto &raRelation = g.getGlobalRelation(ExecutionGraph::RelationId::ra);
 	raRelation = Calculator::GlobalRelation(allEvents);
-}
-
-void VOCalculator::initCalc()
-{	
-	initRaRelation();
-
-	return;
-}
-
-Calculator::CalculationResult VOCalculator::doCalc()
-{
-	llvm::outs() << "--------------- Called doCalc() ---------------\n";
-
-	auto &g = getGraph();
-	//auto &hbRelation = g.getGlobalRelation(ExecutionGraph::RelationId::hb);
-	//auto &pscRelation = g.getGlobalRelation(ExecutionGraph::RelationId::psc);
-	//auto &coRelation = g.getPerLocRelation(ExecutionGraph::RelationId::co);
-	auto &raRelation = g.getGlobalRelation(ExecutionGraph::RelationId::ra);
-
-	calcRaRelation();
-	llvm::outs() << raRelation << "\n";
-
-	return Calculator::CalculationResult(false, false);
-}
-
-void VOCalculator::removeAfter(const VectorClock &preds)
-{
-	/* We do not track anything specific for PSC */
-	return;
 }
